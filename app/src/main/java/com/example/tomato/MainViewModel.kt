41a95033a3d0931165bd7f1f7b4eaeed371681c7
hmc <3241126481@qq.com>
2021-07-02 14:43:27 +0800

    实现基本的计时和取消功能

--- app/src/main/java/com/example/tomato/MainViewModel.kt
+++ app/src/main/java/com/example/tomato/MainViewModel.kt
@@ -3,6 +3,6 @@ package com.example.tomato
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
-    var workTime = 25
+    var workTime = 25//后期改成上次保存的
     var restTime = 5
 }
---
+++ app/src/main/java/com/example/tomato/TomatoFragment.kt
@@ -0,0 +1,142 @@
+package com.example.tomato
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.*
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.ToggleButton
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.fragment_tomato.*
+import java.text.SimpleDateFormat
+import kotlin.concurrent.thread
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class TomatoFragment : Fragment() {
+    //记录状态的变量
+    var myBoolean = true
+
+    companion object {
+        lateinit var handler: Handler
+    }
+
+    lateinit var mainViewModel: MainViewModel
+    var timer1: CountDownTimer? = null
+    var timer2: CountDownTimer? = null
+    var timer3: CountDownTimer? = null
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // Inflate the layout for this fragment
+        mainViewModel.workTime = activity?.intent?.getIntExtra("workTime",
+                25 * 60 * 1000)!!
+        mainViewModel.restTime = activity?.intent?.getIntExtra("restTime",
+                5 * 60 * 1000)!!
+        return inflater.inflate(R.layout.fragment_tomato, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val timeTextView = view.findViewById<TextView>(R.id.time_text_view)
+        timeTextView.text = formatTime(mainViewModel.workTime)
+
+        val statusTextView = view.findViewById<TextView>(R.id.status_text_view)
+
+
+        val toggleButton: ToggleButton = view.findViewById(R.id.toggle_button)
+        toggleButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                myBoolean = true
+                activity?.findViewById<FloatingActionButton>(R.id.fab)?.hide()
+                for (i in 1..3) {
+                    statusTextView.text = "番茄钟已开启"
+                    timer1 = object : CountDownTimer(mainViewModel.workTime.toLong(), 1000) {
+
+                        override fun onTick(millisUntilFinished: Long) {
+                            timeTextView.text = formatTime(millisUntilFinished)
+                            if (!myBoolean) {
+                                cancel()
+                                time_text_view.text = formatTime(mainViewModel.workTime.toLong())
+                            }
+                        }
+
+                        override fun onFinish() {
+                            Toast.makeText(context, "开始休息", Toast.LENGTH_SHORT).show()
+                            statusTextView.text = "休息中"
+                            timer2 = object : CountDownTimer(mainViewModel.restTime.toLong(), 1000) {
+
+                                override fun onTick(millisUntilFinished: Long) {
+                                    timeTextView.text = formatTime(millisUntilFinished)
+                                    if (!myBoolean) {
+                                        cancel()
+                                        time_text_view.text = formatTime(mainViewModel.workTime.toLong())
+                                    }
+                                }
+
+                                override fun onFinish() {
+                                    Toast.makeText(context, "继续", Toast.LENGTH_SHORT).show()
+                                }
+                            }.start()
+                        }
+                    }.start()
+                    timer3 = object : CountDownTimer(mainViewModel.workTime.toLong(), 1000) {
+
+                        override fun onTick(millisUntilFinished: Long) {
+                            timeTextView.text = formatTime(millisUntilFinished)
+                            if (!myBoolean) {
+                                cancel()
+                                time_text_view.text = formatTime(mainViewModel.workTime.toLong())
+                            }
+                        }
+
+                        override fun onFinish() {
+                            Toast.makeText(context, "计时结束", Toast.LENGTH_SHORT).show()
+                        }
+                    }.start()
+                }
+            } else {
+
+                activity?.findViewById<FloatingActionButton>(R.id.fab)?.show()
+                time_text_view.text = formatTime(mainViewModel.workTime.toLong())
+                myBoolean = false
+
+                statusTextView.text = ""
+                activity?.findViewById<FloatingActionButton>(R.id.fab)?.show()
+                if (timer1 != null && timer2 != null) {
+                    timer1!!.cancel()
+                    timer2!!.cancel()
+                    timer3!!.cancel()
+                }
+            }
+        }
+    }
+    fun formatTime(time: Int): String? {
+        val dateFormat = SimpleDateFormat("mm:ss")
+        return dateFormat.format(time)
+    }
+
+    fun formatTime(time: Long): String? {
+        val dateFormat = SimpleDateFormat("mm:ss")
+        return dateFormat.format(time)
+    }
+}
+
+
+
+
+
+
+
